90ec673f684997cd0800361aea8cc07026adee20
Imthath <imthath.m@hotmail.com>
2022-02-05 21:23:08 +0530

    Setup project with relevant packages

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+# Haafidh-iOS 
+
+### To get started
+- Open Terminal in the root folder and run `make app` to generate the project using [Xcodegen](https://github.com/yonaskolb/XcodeGen) 
+
---
+++ create-project.sh
@@ -0,0 +1,14 @@
+APP_NAME="Haafidh"
+
+echo "☠️  Killing Xcode..."
+killall Xcode 2>/dev/null
+
+echo "🧹 Removing project..."
+rm -rf $APP_NAME.xcodeproj
+
+echo "🛠  Regenerating project..."
+xcodegen
+
+# echo "🚀 Opening project!"
+open $APP_NAME.xcodeproj
+# open -a "AppCode" $APP_NAME.xcodeproj
