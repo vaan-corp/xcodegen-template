5d26c3eeb180ef7d67127e03c0d11452fbe2ef51
Imthath <imthath.m@hotmail.com>
2023-01-20 16:16:37 +0530

    macOS: Setup windows

--- macOS/AppDelegate.swift
+++ macOS/AppDelegate.swift
@@ -7,15 +7,25 @@
 //
 
 import Cocoa
+import SwiftUI
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-  
-
+  var window: NSWindow!
 
   func applicationDidFinishLaunching(_ aNotification: Notification) {
     // Insert code here to initialize your application
+    window = NSWindow(
+      contentRect: NSRect(x: 0, y: 0, width: 1600, height: 900),
+      styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+      backing: .buffered, defer: false)
+    window.isReleasedWhenClosed = false
+    window.center()
+    window.setFrameAutosaveName("Main Window")
+
+    window.contentViewController = NSHostingController(rootView: ContentView())
+    window.makeKeyAndOrderFront(nil)
+    window.setFrame(NSRect(x: 0, y: 0, width: 1600, height: 900), display: true)
   }
 
   func applicationWillTerminate(_ aNotification: Notification) {
@@ -25,7 +35,5 @@ class AppDelegate: NSObject, NSApplicationDelegate {
   func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
     return true
   }
-
-
 }
 
